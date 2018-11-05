ab0369f7f65359783ca2ba7a089063ebc047c907
Yashas ND <yashasbharadwaj111@gmail.com>
2018-11-05 12:10:38 +0530

    Sriniketh's version of BoardPos compatible with legacy

--- boardpos.py
+++ boardpos.py
@@ -1,14 +1,14 @@
-class Boardpos:
-	def __init__(self):
-        	self.a = [[0 for i in range(0,7)] for j in range(0,6)]
+# class Boardpos:
+# 	def __init__(self):
+#         	self.a = [[0 for i in range(0,7)] for j in range(0,6)]
 	    
 #please use the below class
 
-#class Boardpos:
-#	def __init__(self):
- #              self.a = [[0 for i in range(0,7)] for j in range(0,6)]
-#		self.du=0
-#		self.th=0
-#		self.fr=0
-#		self.ftb=0
-#		sefl.fblk=0
+class Boardpos:
+	def __init__(self):
+              self.a = [[0 for i in range(0,7)] for j in range(0,6)]
+              self.du=0
+              self.th=0
+              self.fr=0
+              self.ftb=0
+              self.fblk=0
