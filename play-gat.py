6b380cdda37df7d8f391e22ff4aec54b64bc7166
Yashas ND <yashasbharadwaj111@gmail.com>
2018-11-18 16:59:42 +0530

    Added the AI vs Human module to playe with AI

---
+++ play-gat.py
@@ -0,0 +1,33 @@
+import time
+import arena
+import pickle
+import referee
+import gametree
+import boardpos
+import movemaker
+import ga2.gaDisc as gad
+
+PLANK_DEPTH = 3
+
+if(__name__=="__main__"):
+    fd = open("BAGENT.dna", 'rb')
+    playerObj1 = pickle.load(fd)
+    fd.close()
+    gameOver = False
+    player = 1
+    root = gametree.TreeNode(parent=None, player=player)
+    root.makeTree(PLANK_DEPTH)
+    board = boardpos.Boardpos()
+    while(not gameOver):
+        oldpos = root.boardpos
+        move, root = movemaker.acceptmove(root, player)
+        gameOver = arena.change_vals(oldpos, move, player)
+        player = gametree.toggle(player)
+        
+        if(not gameOver):
+            oldpos = root.boardpos
+            move, root = movemaker.move(playerObj1, root, player)
+            gameOver = arena.change_vals(oldpos, move, player)
+            player = gametree.toggle(player)
+            if(gameOver):
+                print("Agent {} won with fitness {}".format(playerObj2.agentID, playerObj2.fitness))
